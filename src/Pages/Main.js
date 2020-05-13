e87e63d1c8b1e3218000fb213b56e6907d6958c2
Griffin Poole <griffinpoole5@gmail.com>
2020-05-12 21:25:10 -0400

    Layed out div framework and styled, added some initial portions of the Main page. Additions beyonond this commit may not be initially dynamic

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { Header } from 'semantic-ui-react'
 
 function Main(props) {
     return (
-        <Link to='/project/yeet'>Test Project: Yeet</Link>
+        <>
+            <div style={{position:'absolute',  top:'1%', left:'1%', height:'53%', width:'100%', backgroundColor:'#80808078'}}/>
+            <div className='buisnessCard' style={{position:'absolute', height:'53%', width:'100%', backgroundColor:'rgb(239, 233, 233)'}}>
+                <div style={{paddingTop:'3em', paddingLeft:'2em', paddingRight:'2em'}}>
+                    <Header as='h1'
+                            style={{fontFamily:'Verdana, Helvetica', 
+                                    fontWeight:'bold', 
+                                    fontSize:'2.5em',
+                                    marginBottom:0,
+                                    color:'#353535'}}>Griffin Poole</Header>
+                    <Header.Subheader as='h3' 
+                                    style={{marginTop:0, 
+                                            color:'#757577'}}>Full Stack Web Developer, Neuroscience BA</Header.Subheader>
+                </div>
+
+            </div>
+            <div style={{position:'absolute',  top:'56%', left:'1%', height:'45%', width:'100%', backgroundColor:'#80808078'}}/>
+            <div className='projectsCard' style={{position:'absolute', top:'55%', height:'45%', width:'100%', backgroundColor:'rgb(239, 233, 233)'}}>
+                
+            </div>
+        </>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -14,10 +14,23 @@ import './CSS/index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route exact path='/' component={Main}/>
-        <Route path='/project/:name' component={Project}/>
-      </Switch>
+      <div style={{height:'200vh', width:'100%', display: 'flex', flexDirection:'column'}}>
+          <div className='background' style={{zIndex:'-2', width:'100%', backgroundColor:'#d69412e8'}}>
+            <img src='isaac-smith-6EnTPvPPL6I-unsplash.jpg' style={{width:'100%'}}></img>
+          </div>
+          <div style={{zIndex:'-1', flexGrow:'1', width:'100%', backgroundColor:'#d69412e8', position:'relative'}}>
+            <div className='content' style={{width:'85%',
+                                             position:'relative',
+                                             bottom:'5em',
+                                             height:'100%',
+                                             left:'7.5%'}}>
+              <Switch>
+                <Route exact path='/' component={Main}/>
+                <Route path='/project/:name' component={Project}/>
+              </Switch>
+            </div>
+          </div>
+      </div>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
