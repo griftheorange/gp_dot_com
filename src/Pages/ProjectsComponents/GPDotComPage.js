8136f32cf837c43050fc6c5695404dfb9ca75c5e
Griffin Poole <griffinpoole5@gmail.com>
2020-05-15 22:27:04 -0400

    Put titlecard templates in remaining pages, need to update data

--- src/Pages/ProjectsComponents/GPDotComPage.js
+++ src/Pages/ProjectsComponents/GPDotComPage.js
@@ -3,7 +3,31 @@ import React from 'react';
 function GPDotComPage(props) {
     return (
         <div className={'project-desc-wrapper'}>
-            Website
+            <TitleCard>
+                <TitleLeftbar title={'Budget.web'}
+                              subtitle={'Personal Finance App'}
+                              listHash={{
+                                  'Javascript - Frontend':[
+                                      'React.js',
+                                      'Chart.js',
+                                      'Redux',
+                                      'React Sidebar',
+                                      'Semantic-UI'
+                                  ],
+                                  'Python - Backend':[
+                                      'Flask',
+                                      'Numpy',
+                                      'Pandas',
+                                      'Anaconda',
+                                      'Xlrd'
+                                  ]
+                              }}
+                              github={{
+                                  'frontend':'https://github.com/griftheorange/budget_web_py_frontend',
+                                  'backend':'https://github.com/griftheorange/budget_web_py_backend'
+                              }}/>
+                <TitleRightbar gif={budgetPyAnimation1}/>
+            </TitleCard>
         </div>
     );
 }
