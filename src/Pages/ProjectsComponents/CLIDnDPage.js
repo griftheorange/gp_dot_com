1486ac2c3c82c12cb43cc3c670c1b55abbbd3864
Griffin Poole <griffinpoole5@gmail.com>
2020-05-18 22:49:46 -0400

    Beginnings of CLI DnD page complete

--- src/Pages/ProjectsComponents/CLIDnDPage.js
+++ src/Pages/ProjectsComponents/CLIDnDPage.js
@@ -1,37 +1,36 @@
 import React from 'react';
 
+import TextBlocks from './SupportComponents/TextBlocks.js'
 import TitleCard from './SupportComponents/TitleCard.js'
 import TitleLeftbar from './SupportComponents/TitleLeftbar.js'
 import TitleRightbar from './SupportComponents/TitleRightbar.js'
-import budgetPyAnimation1 from '../../Resources/ProjectAnimations/budgetPyAnimation1.gif'
+import SummaryBlock from './SupportComponents/SummaryBlock.js'
+import FeatureBlock from './SupportComponents/FeatureBlock.js'
+import ComponentTree from './SupportComponents/ComponentTree.js'
+
+import CLIDnDAnimation1 from '../../Resources/ProjectAnimations/CLIDnDAnimation1.gif'
 
 function CLIDnDPage(props) {
     return (
         <div className={'project-desc-wrapper'}>
             <TitleCard>
-                <TitleLeftbar title={'Budget.web'}
-                              subtitle={'Personal Finance App'}
+                <TitleLeftbar title={'CLI DnD'}
+                              subtitle={'DnD Organizer, Monster Database CLI'}
                               listHash={{
-                                  'Javascript - Frontend':[
-                                      'React.js',
-                                      'Chart.js',
-                                      'Redux',
-                                      'React Sidebar',
-                                      'Semantic-UI'
-                                  ],
-                                  'Python - Backend':[
-                                      'Flask',
-                                      'Numpy',
-                                      'Pandas',
-                                      'Anaconda',
-                                      'Xlrd'
+                                  'Ruby':[
+                                      'Sinatra',
+                                      'Active Record',
+                                      'SQLite3',
+                                      'Rest Client',
+                                      'Launchy',
+                                      'Rake'
                                   ]
                               }}
                               github={{
-                                  'frontend':'https://github.com/griftheorange/budget_web_py_frontend',
-                                  'backend':'https://github.com/griftheorange/budget_web_py_backend'
-                              }}/>
-                <TitleRightbar gif={budgetPyAnimation1}/>
+                                  'repo':'https://github.com/griftheorange/module-one-final-project-guidelines-dumbo-web-102819'
+                              }}
+                              demo={'https://youtu.be/rx2kztJJ9mc'}/>
+                <TitleRightbar gif={CLIDnDAnimation1}/>
             </TitleCard>
         </div>
     );
