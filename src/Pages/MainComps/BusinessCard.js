dee7980c47c4ccf563f636be6a18f648640d8ab2
Griffin Poole <griffinpoole5@gmail.com>
2020-06-04 22:58:18 -0400

    Changes summary block to display full content on mobile without scroll

--- src/Pages/MainComps/BusinessCard.js
+++ src/Pages/MainComps/BusinessCard.js
@@ -4,10 +4,14 @@ import profile from '../../Resources/DSC_1989.JPG'
 import Contacts from './BusinessComponents/Contacts'
 import Skillsets from './BusinessComponents/Skillsets';
 import Description from './BusinessComponents/Description'
+import {useBreakpoint} from '../useBreakpoint'
 
 function BusinessCard(props) {
+
+    const breakpoint = useBreakpoint()
+
     return (
-        <div className={'business-card-container'}>
+        <div className={breakpoint['lg'] ? 'business-card-container lg' : 'business-card-container'}>
             <img className={'profile-img'} src={profile}/>
             <Contacts/>
             <Skillsets/>
